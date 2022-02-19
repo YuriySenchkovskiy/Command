fa427e6ee4f2ce16096e91dcc1cc30022898aced
Yuriy <yuriy.senchkovskiy@gmail.com>
2022-02-19 18:35:51 +0300

    1.1.1

--- Assets/Script/DropDownHandler.cs
+++ Assets/Script/DropDownHandler.cs
@@ -1,18 +1,15 @@
 using System;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Script
 {
     public class DropDownHandler : MonoBehaviour
     {
-        [SerializeField] private ChangeButtonEventComponent _action;
-
-        private Dropdown drop;
         void Start()
         {
-            drop = transform.GetComponent<Dropdown>();
+            var input = FindObjectOfType<InputHandler>();
+            var drop = transform.GetComponent<Dropdown>();
             drop.options.Clear();
 
             var keyCodes  = Enum.GetNames(typeof(KeyCode));
@@ -22,18 +19,8 @@ namespace Script
                 drop.options.Add(new Dropdown.OptionData() {text = k});
             }
 
-            DropDownItemsSend(drop);
-            drop.onValueChanged.AddListener(delegate { DropDownItemsSend(drop); });
+            input.ChangeGoForwardButton(drop.value);
+            drop.onValueChanged.AddListener(input.ChangeGoForwardButton);
         }
-
-        private void DropDownItemsSend(Dropdown drop)
-        {
-            _action?.Invoke(drop.value);
-        }
-    }
-    
-    [Serializable]
-    public class ChangeButtonEventComponent : UnityEvent<Int32>
-    {
     }
 }
